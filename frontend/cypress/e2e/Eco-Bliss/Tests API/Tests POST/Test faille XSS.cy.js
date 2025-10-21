44158fd3ca2feaffc852d228c9acdc80ef82e7b5
Rionecnirp <alex.ben-larbi@hotmail.fr>
2025-10-21 15:03:49 +0200

    21/10/2025 - 15h00 : Ajout du test pour la faille XSS

---
+++ frontend/cypress/e2e/Eco-Bliss/Tests API/Tests POST/Test faille XSS.cy.js
@@ -0,0 +1,44 @@
+describe('Tests API - Faille XSS', () => {
+
+    it('Ne doit pas accepter ni renvoyer de code HTML ou script brut', () => {
+
+        cy.login().then((token) => {
+
+            const review = {
+                rating: 3,
+                title: 'Test faille XSS',
+                comment: '<script>alert("XSS")</script>', 
+            }
+
+            cy.request({
+                method: 'POST',
+                url: 'http://localhost:8081/reviews',
+                failOnStatusCode: false,
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                },
+                body: review,
+            }).then((response) => {
+
+                cy.log('Réponse :', JSON.stringify(response.body));
+
+                if ([400, 403].includes(response.status)) {
+                    expect([400, 403].toLocaleString.include(response.status))
+                }
+
+                else if (response.status === 200) {
+                    expect(response.body.comment).to.not.include('<script>');
+                    expect(response.body.comment).to.not.include('</script>');
+                }
+
+                else {
+                throw new Error(`Réponse inattendue : ${response.status}`);
+                }
+            });
+
+        });
+
+    });
+
+});
